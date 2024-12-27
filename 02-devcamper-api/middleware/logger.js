ae06065ab4fa0e7d2c8b7113c8f7960b8e4d2b09
Sandeep <sandeep.tanjore@mcgill.ca>
2024-12-27 12:13:10 -0500

    Continuing with Middleware and Error Handling

---
+++ 02-devcamper-api/middleware/logger.js
@@ -0,0 +1,7 @@
+//@description 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+  next();
+}
+
+module.exports = logger;
--- 02-devcamper-api/server.js
+++ 02-devcamper-api/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+//const logger = require("./middleware/logger");
 const morgan = require("morgan");
 const colors = require("colors");
 const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+
 
 
 //Load env variables
@@ -16,6 +19,11 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+//app.use(logger);
+
+//Body parser
+app.use(express.json());
+
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan("dev"));
@@ -23,7 +31,7 @@ if (process.env.NODE_ENV === 'development') {
 
 //Mount Router
 app.use("/api/v1/bootcamps", bootcamps);
-
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
